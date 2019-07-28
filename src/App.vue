671f0ad5cb7e899f810bb2780e53ab4fb4dfaddf
takuya takahashi <tkhotuch@me.com>
2019-07-28 20:31:32 +0900

    DEMOに画像ダウンロードリンクを追加

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,23 @@
     resize: <input type="number" v-model.number="maxLength">px
     </div>
     <div class="item">
-    <ImagePickAndResizer 
-      v-model="imageFile1"
-      :maxLength="maxLength"/>  
-      {{imageFile1}}
-
+      <ImagePickAndResizer 
+        v-model="imageFile1"
+        :maxLength="maxLength"/>  
+      <div v-if="imageFile1">
+        {{imageFile1.size | byteNumber}}
+        <a v-bind:href="imageFile1 | downloadLink" target="_blank">Download</a>
+      </div>
     </div>
     <div class="item">
-    <ImagePickAndResizer 
-      v-model="imageFile2"
-      :maxLength="maxLength"/>  
-      {{imageFile2}}
+      <ImagePickAndResizer 
+        v-model="imageFile2"
+        :maxLength="maxLength"/>  
+      <div v-if="imageFile2">
+        {{imageFile2.size | byteNumber}}
+        <a v-bind:href="imageFile2 | downloadLink" target="_blank">Download</a>
+      </div>
+
     </div>  
   </div>
  </template>
@@ -24,10 +30,26 @@
 import { Component, Vue } from 'vue-property-decorator';
 import ImagePickAndResizer from './components/ImagePickAndResizer.vue';
 import {pathToFile} from 'image-to-file-converter';
+import * as Numeral from 'numeral'
 @Component({
   components: {
     ImagePickAndResizer,
   },
+  /** filters */
+  filters: {
+    byteNumber(value: number): string | null {
+      if (!value) {
+        return null
+      }
+      return Numeral(value).format('0 b')
+    },
+    downloadLink(value: File): string | null {
+      if (!value) {
+        return null
+      }
+      return window.URL.createObjectURL(value)
+    }
+  }  
 })
 export default class App extends Vue {
   /** Data */
